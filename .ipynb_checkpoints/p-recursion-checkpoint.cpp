cbaeb36c5ba43da11e4d1bb90cc06d1c42c9d9fe
Alice Schwarze <aliceschwarze@gmail.com>
2024-07-05 17:35:23 -0400

    close debug

---
+++ .ipynb_checkpoints/p-recursion-checkpoint.cpp
@@ -0,0 +1,134 @@
+//#include <pybind11/pybind11.h>
+#include <cmath>
+#include <iostream>
+#include <unordered_map>
+#include <cstdlib> // for command line input
+#include <string> // for std::string
+#include <sstream> // for std::stringstream
+#include <chrono> // for timing
+
+//namespace py = pybind11;
+
+// Function to calculate the factorial
+int factorial(int n) {
+    if (n <= 1)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+// Function to calculate the combination (old)
+double comb_old(int n, int r) {
+    std::cout << "comb" << n << r << std::endl;
+    return factorial(n) / (factorial(r) * factorial(n - r));
+}
+
+// Function to calculate the combination
+long long comb(long long n, long long r) 
+{
+    long long f = 1; 
+    for(auto i = 0; i < r;i++)
+        f = (f * (n - i)) / (i + 1);
+    return f ; 
+}
+
+//Calculate g(p,i,n) for calculate_P_mult
+double calculate_g(double p, int i, int n) {
+    double g = pow(1 - p, i * (n - i));
+    return g;
+}
+
+//Calculate f(p,i,n) for calculate_P_mult
+double calculate_f(double p, int i, int n, std::unordered_map<double, std::unordered_map<int, std::unordered_map<int, double>>>& fdict) {
+    if (fdict.find(p) != fdict.end() && fdict[p].find(n) != fdict[p].end() && fdict[p][n].find(i) != fdict[p][n].end()) {
+        return fdict[p][n][i];
+    } else {
+        double sum_f = 0;
+        for (int i_n = 1; i_n < i; ++i_n) {
+            sum_f += (calculate_f(p, i_n, n, fdict) * comb(i - 1, i_n - 1) * pow(1 - p, i_n * (i - i_n)));
+        }
+        double f = 1 - sum_f;
+        fdict[p][n][i] = f; // does that change the original?
+        return f;
+    }
+}
+
+// Calculate h(p,i,n,k) for calculate_P_mult
+double calculate_h(double p, int i, int n, int k, std::unordered_map<double, std::unordered_map<int, std::unordered_map<int, double>>>& fdict) {
+    double h = comb((n - (k - 1) * i), i) * calculate_f(p, i, n, fdict) * calculate_g(p, i, (n - (k - 1) * i));
+    return h;
+}
+
+// Calculate probability of a G(n,p) graph to have a largest connected component with i nodes. Calculation accounts for the possibility of multiple largest connected components.
+double calculate_P_mult(double p, int i, int n, std::unordered_map<double, std::unordered_map<int, std::unordered_map<int, double>>>& fdict, std::unordered_map<double, std::unordered_map<int, std::unordered_map<int, double>>>& pdict) {
+    if (i == 1 && n == 1) {
+        return 1;
+    } else if (i == 1 && n != 1) {
+        return pow(1 - p, comb(n, 2));
+    } else {
+        double P_tot = 0;
+        for (int k = 1; k <= n / i; ++k) {
+            double product = 1;
+            for (int k_2 = 1; k_2 <= k; ++k_2) {
+                product *= calculate_h(p, i, n, k_2, fdict);
+            }
+
+            double sum_less = 0;
+            for (int j = 1; j < i; ++j) {
+                sum_less += calculate_P_mult(p, j, n - k * i, fdict, pdict);
+            }
+
+            P_tot += 1.0 / factorial(k) * product * sum_less;
+        }
+        return P_tot;
+    }
+}
+
+int timer() { //toggle to main if needed
+    // Define input values for the loop
+    int inputValues[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50};
+
+    // Loop through each input value
+    for (int i = 0; i < sizeof(inputValues) / sizeof(inputValues[0]); ++i) {
+        // Get the current input value
+        int inputValue = inputValues[i];
+
+        // Timing setup
+        auto startTime = std::chrono::high_resolution_clock::now();
+
+        // Call the function
+        std::unordered_map<double, std::unordered_map<int, std::unordered_map<int, double>>> fdict, pdict;
+        double result = calculate_P_mult(0.2, inputValue, 2*inputValue, fdict, pdict);
+    
+        // Timing calculation
+        auto endTime = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+
+        // Output timing information
+        std::cout << "calculate_P_mult(0.2," << inputValue << "," << 2*inputValue << ") returned " << result <<" after " << duration.count() << " microseconds" << std::endl;
+    }
+
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    // Check if the correct number of command line arguments are provided
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " p, i, n" << std::endl;
+        return 1;
+    }
+
+    // Parse command line arguments
+    double p = std::strtod(argv[1], nullptr);
+    int i = std::atoi(argv[2]);
+    int n = std::atoi(argv[3]);
+    std::unordered_map<double, std::unordered_map<int, std::unordered_map<int, double>>> fdict, pdict;
+    double result = calculate_P_mult(p, i, n, fdict, pdict);
+    std::cout << result << std::endl;
+}
+
+
+//PYBIND11_MODULE(recursion, m) {
+//    m.doc() = "submodule for fast recursive calculation of probabilities in c++"; 
+
+//    m.def("calculate_P_full", &calculate_P_full, "A function that calculates probability of a G(n,p) graph to have a largest connected component with i nodes.");
+//}
--- libs/.ipynb_checkpoints/finiteTheory-checkpoint.py
+++ libs/.ipynb_checkpoints/finiteTheory-checkpoint.py
@@ -83,12 +83,10 @@ def raw_f(p, i, n):
        The probability that a subgraph with `i` nodes of an Erdos--Renyi
        random graph with `n` nodes and edge probability `p` is connected.
     '''
-    # if i == 0:
-    #     p_connect = 0
+
     if i == 1:
         p_connect = 1
-    # elif i > n:
-    #     p_connect = 0
+
     else:
         sum_f = 0
         for i_n in range(1, i, 1):
@@ -130,14 +128,6 @@ def calculate_f(p, i, n, fdict={}): # using dictionary to calculate f values
             if i in fdict[p][n]:
                 return fdict[p][n][i]
 
-    # if none is found start computation
-    # if i == 0:
-    #     f = 0
-    # if i == 1:
-    #     f = 1
-    # # elif i>n:
-    # #     f = 0
-    # else:
     sum_f = 0
     for i_n in range(1, i, 1):
         sum_f += (calculate_f(p, i_n, n, fdict=fdict)
@@ -199,18 +189,12 @@ def raw_P(p, i, n):
        The probability that an Erdos--Renyi random graph with `n` nodes and 
        edge probability `p` has a largest connected component of size `i`.
     '''
-    # if i == 0 and n == 0:
-    #     P_tot = 1
-    # if i > 0 and n == 0:
-    #     P_tot = 0
-    # if i > n or n < 0 or i <= 0:
-    #     P_tot = 0
+
     if i == 1 and n == 1:
         P_tot = 1
     elif i == 1 and n != 1:
         P_tot = (1 - p) ** comb(n, 2)
-    # elif i == n:
-    #     P_tot = raw_f(p,n,n)
+
     else:
         sum_P = 0
         for j in range(0, i + 1, 1):
@@ -256,28 +240,14 @@ def calculate_P(p, i, n, fdict={}, pdict={}): # find P with dictionary
             if i in pdict[p][n]:
                 return pdict[p][n][i]
 
-    # if i == 0 and n == 0:
-    #     P_tot = 1
-    # if i > 0 and n == 0:
-    #     P_tot = 0
-    # if i > n or n < 0 or i <= 0:
-    #     P_tot = 0
     if i == 1 and n == 1:
         P_tot = 1
     elif i == 1 and n != 1:
         P_tot = (1 - p) ** comb(n, 2)
-    # elif i == n:
-    #     P_tot = calculate_f(p,n,n)
+
     else:
         sum_P = 0
 
-        # 1-sum (P(p,j,n)) for j=i+1 to n
-        # for j in range(i+1, n+1, 1):
-        #     sum_P += calculate_P(p, j, n - i, fdict=fdict, pdict=pdict)
-        # P_tot = (scipy.special.comb(n, i) * calculate_f(p, i, n, fdict=fdict)
-        #          * calculate_g(p, i, n) * (1 - sum_P))  # * factor of ceiling(n/2)??
-        #
-        # normal way with j = 1 to i
         for j in range(1, i + 1, 1):
             if j==i:
                 sum_P += .5 * calculate_P(p, j, n - i, fdict=fdict, pdict=pdict)
@@ -290,144 +260,17 @@ def calculate_P(p, i, n, fdict={}, pdict={}): # find P with dictionary
     return P_tot
 
 
-def calcA(p,i,n): # calculate P_tot without j=i
-    P_tot = 0
-    for j in range(i):
-        P_tot+=calculate_P(p,j,n-i)
-    P_tot = scipy.special.comb(n,i)*calculate_f(p,i,n)*calculate_g(p,i,n)*P_tot
-    return P_tot
-
-def calcB(p,i,n): # calculate P_tot for only j=i
-    P_tot = scipy.special.comb(n,i)*calculate_f(p,i,n)*calculate_g(p,i,n)*calculate_P(p,i,n-i)
-    return P_tot
-
-def calcC(p,n): # find the factor C such that sum of calcA + calcB * c over all j<=i = 1
-    A = 0
-    B = 0
-    for i_i in range(1,n+1):
-        A+=calcA(p,i_i,n)
-        B+=calcB(p,i_i,n)
-    if B == 0:
-        c = 0
-    else:
-        c = (1 - A) / B
-    return c
-
-
-def abc(p,i,n): # make an array with P(p,i,n) for i<=n
-    # use calcA, calB, calcC
-  a_table = np.zeros(i+1)
-  b_table = np.zeros(i+1)
-  abc_table = np.zeros(i+1)
-
-  A = 0
-  B = 0
-  for i_i in range(1,n+1):
-    a_table[i_i] = calcA(p,i_i,n)
-    b_table[i_i] = calcB(p,i_i,n)
-    A+=a_table[i_i]
-    B+=b_table[i_i]
-  if B == 0:
-    c = 0
-  else:
-    c = (1-A)/B
-  for j in range(1,n+1):
-    # find P(p,j,n) with the equation: calcA(p,j,n)+calcC(p,j,n)*calcB(p,j,n) = P(p,j,n)
-    abc_table[j] = a_table[j] + c*b_table[j]
-  return abc_table
-
-
-def c_graph(n,p,to_vary): # graph c over n or p for various levels of p or n respectively
-    if to_vary == "n":
-        for i_p in range(len(p)):
-            c_vals = np.zeros(n[0]+1)
-            n_vals = np.linspace(0,n[0],n[0]+1)
-            for i_n in range(n[0]+1):
-                c_vals[i_n] = calcC(p[i_p],i_n)
-            plt.plot(n_vals, c_vals, label="p="+str(p[i_p]))
-            print("plotted")
-        print(n_vals)
-        print(c_vals)
-        plt.xlabel("n")
-        plt.ylabel("c")
-
-    elif to_vary == "p":
-        for i_n in range(len(n)):
-            print(n[i_n])
-            p_vals = np.linspace(0, 1, 81, endpoint=True)
-            c_vals = np.zeros(len(p_vals))
-            for i_p in range(11):
-                c_vals[i_p] = calcC(p_vals[i_p],n[i_n])
-            plt.plot(p_vals, c_vals, label="n="+str(n[i_n]))
-            print("plotted")
-        print(p_vals)
-        print(c_vals)
-        plt.xlabel("p")
-        plt.ylabel("c")
-    "should show"
-    plt.legend()
-    plt.show()
-
-
-def normalized(p,i,n):
-  # calculates P for a certain i and n
-  # normalizes the P values during the calculation rather than just at the end
-
-  p_vals = np.zeros(n+1)
-  for i_i in range(n+1):
-    print(i_i)
-    # if i_i == 0 and n == 0:
-    #   P_tot = 1
-    # if i_i > 0 and n == 0:
-    #       P_tot = 0
-    # if i_i > n or n < 0 or i_i <= 0:
-    #       P_tot = 0
-    if i_i == 1 and n != 1:
-          P_tot = (1 - p) ** comb(n, 2)
-    # elif i_i == n:
-    #   P_tot = calculate_f(p,i_i,n)
-    else:
-      P_tot = 0
-      for j in range(1,i_i+1):
-          if j==i:
-            P_tot+=.5*normalized(p,j,n-i_i)
-          else:
-            P_tot += normalized(p, j, n - i_i)
-      P_tot = scipy.special.comb(n,i_i)*calculate_f(p,i_i,n)*calculate_g(p,i_i,n)*P_tot
-    p_vals[i_i] = P_tot
-  c = np.sum(p_vals)
-  if i<=n:
-    return p_vals[i]/c
-  else:
-    return 0
-
-def normalized_table(p,i,n):
-    # creates table of P probabilities that are normalized during the calculations
-
-  p_table = np.zeros(shape=(n+1,i+1))
-  for i_n in range(n+1):
-    for i_i in range(i+1):
-      p_table[i_n,i_i] = normalized(p,i_i,i_n)
-  return p_table
-
 
 def alice_helper(p,i,n,k,fdict={},pdict={}):
     # (n - k*i choose i) * f * g(p,i,n-k*i)
     return scipy.special.comb((n-(k-1)*i),i)*calculate_f(p, i, n, fdict=fdict)* calculate_g(p, i, (n-(k-1)*i))
 
 def alice(p,i,n,fdict={},pdict={}):
-    # if i == 0 and n == 0:
-    #     P_tot = 1
-    # if i > 0 and n == 0:
-    #     P_tot = 0
-    # if i > n or n < 0 or i <= 0:
-    #     P_tot = 0
     if i == 1 and n == 1:
         P_tot = 1
     elif i == 1 and n != 1:
         P_tot = (1 - p) ** comb(n, 2)
-    # elif i == n:
-    #     P_tot = calculate_f(p,n,n)
+
     else:
         P_tot = 0
         for k in range(1,n//i + 1): # each exact number of lcc's to calculate P for
@@ -481,14 +324,14 @@ def calculate_P_mult(p, i, n, executable_path="p-recursion.exe"):
     # Execute the executable and capture its output
     # print(os. getcwd())
     output = float(execute_executable([executable_path, str(p), str(i), str(n)]))
-    #print("EEO output", output)
+    print(p, i, n, "EEO output", output)
     #output = float(output)
 
     # return
     return output
 
 
-def calculate_S(p, n, fdict={}, pdict={},lcc_method = "abc"):
+def calculate_S(p, n, fdict={}, pdict={},lcc_method = "pmult"):
     '''Load or compute the expected size of the largest connected component of
     an Erdos--Renyi random graph with `n` nodes and edge probability `p` using
     equations for percolation in finite networks.
@@ -513,21 +356,7 @@ def calculate_S(p, n, fdict={}, pdict={},lcc_method = "abc"):
        Expected size of the largest connected component of an Erdos--Renyi
        random graph with `n` nodes and edge probability `p`.
     '''
-    if lcc_method == "abc":
-        S = 0
-        p_table = abc(p, n, n)
-        for k in range(1, n + 1):
-            S += p_table[k] * k
-        return S
-
-    elif lcc_method == ".5 factor":
-        S = 0
-        for k in range(1, n + 1):
-            S += k*calculate_P(p,k,n)
-            #S += k*raw_P(p,k,n)
-        return S
-
-    elif lcc_method == "alice":
+    if lcc_method == "alice":
         S=0
         for m in range(1,n+1):
             S+=m*alice(p,m,n,fdict=pdict,pdict=pdict)
@@ -539,15 +368,8 @@ def calculate_S(p, n, fdict={}, pdict={},lcc_method = "abc"):
             S+=m*calculate_P_mult(p,m,n)
         return S
 
-    elif lcc_method == "normalized during":
-        S = 0
-        p_table = normalized_table(p, n, n)
-        for k in range(1, n + 1):
-            S += p_table[n, k] * k
-        return S
 
-
-def SCurve(p, n, attack=False, reverse=False, fdict={}, pdict={}, lcc_method_Scurve="abc"):
+def SCurve(p, n, attack=False, reverse=False, fdict={}, pdict={}, lcc_method_Scurve="pmult"):
     '''Sequence of the expected sizes of the largest connected component of
     an Erdos--Renyi random graph with `n` nodes and edge probability `p` when
     removing nodes sequentially, either uniformly at random or (adaptively) 
@@ -605,7 +427,7 @@ def SCurve(p, n, attack=False, reverse=False, fdict={}, pdict={}, lcc_method_Scu
     return S
 
 
-def relSCurve(p, n, attack=False, reverse=True, fdict={}, pdict={}, lcc_method_relS = "abc"):
+def relSCurve(p, n, attack=False, reverse=True, fdict={}, pdict={}, lcc_method_relS = "pmult"):
     '''Sequence of the expected relative sizes of the largest connected 
     component of an Erdos--Renyi random graph with `n` nodes and edge 
     probability `p` when removing nodes sequentially, either uniformly at
