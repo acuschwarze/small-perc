b7724b3054de5b07c37ca548d7b32b7780996427
jj2189 <110835433+jj2189@users.noreply.github.com>
2024-08-14 09:04:32 -0400

    fig 4 file update (plot a 3d surface)
    it looks weird but we can work on that!

--- Figure 4.py
+++ Figure 4.py
@@ -7,6 +7,7 @@ import time # package to help keep track of calculation time
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
+import matplotlib.tri as mtri
 import pandas as pd
 
 import scipy
@@ -29,22 +30,49 @@ fvals = pickle.load(open('data/fvalues.p', 'rb'))
 pvals = pickle.load(open('data/Pvalues.p', 'rb'))
 
 fullData = pd.read_csv("fullData.csv")
-k = len(fullData)
 
-mse_array = np.zeros((k,4),dtype=object)
+# making csv of data
 
-for i in range(k):
-    n = fullData.iloc[i][1]
-    p = fullData.iloc[i][2] / scipy.special.comb(n,2)
-    sim = string2array(fullData.iloc[i][3], sep=" ")
-    fin = string2array(fullData.iloc[i][5], sep=" ")
-    mse = ((fin-sim)**2).mean()
+# k = len(fullData)
 
-    mse_array[i][0] = fullData.iloc[i][0]
-    mse_array[i][1] = n
-    mse_array[i][2] = p
-    mse_array[i][3] = mse
+# mse_array = np.zeros((k,4),dtype=object)
 
-df = pd.DataFrame(mse_array)
-df.to_csv("MSEdata3D.csv")
-df.columns = ["network", "n", "p", "mse"]
+# for i in range(k):
+#     n = fullData.iloc[i][1]
+#     p = fullData.iloc[i][2] / scipy.special.comb(n,2)
+#     sim = string2array(fullData.iloc[i][3], sep=" ")
+#     fin = string2array(fullData.iloc[i][5], sep=" ")
+#     mse = ((fin-sim)**2).mean()
+
+#     mse_array[i][0] = fullData.iloc[i][0]
+#     mse_array[i][1] = n
+#     mse_array[i][2] = p
+#     mse_array[i][3] = mse
+
+# df = pd.DataFrame(mse_array)
+# df.to_csv("MSEdata3D.csv")
+# df.columns = ["network", "n", "p", "mse"]
+
+
+# making 3D graph
+msedata = pd.read_csv("MSEdata3D.csv")
+num_nwks = len(msedata)
+nodes_array = np.zeros(num_nwks)
+probs_array = np.zeros(num_nwks)
+mse_array = np.zeros(num_nwks)
+
+# some weird formatting means you have to add 1 to each index for the n,p,mse
+for j in range(num_nwks):
+    nodes_array[j] = msedata.iloc[j][2]
+    probs_array[j] = msedata.iloc[j][3]
+    mse_array[j] = msedata.iloc[j][4]
+
+fig = plt.figure()
+ 
+ax = plt.axes(projection ='3d')
+ 
+#ax.scatter(nodes_array, probs_array, mse_array)
+tri = mtri.Triangulation(nodes_array, probs_array)
+ax.plot_trisurf(nodes_array, probs_array, mse_array, triangles=tri.triangles, cmap=plt.cm.Spectral)
+ax.set_title('MSE over n and p')
+plt.show()
