7c5c55c6308f9305ad647c1b848d5f7bf7998bb0
jj2189 <110835433+jj2189@users.noreply.github.com>
2024-08-14 20:27:11 -0400

    updated colorbars heatmaps

--- Figure 3.py
+++ Figure 3.py
@@ -29,7 +29,7 @@ fvals = pickle.load(open('data/fvalues.p', 'rb'))
 pvals = pickle.load(open('data/Pvalues.p', 'rb'))
 
 
-total_n = 60
+total_n = 30
 total_p = 100
 nodes_array = np.arange(1,total_n+1)
 probs_array = np.linspace(.01,1,total_p)
@@ -78,6 +78,11 @@ for i in range(1,total_n+1):
         fin = (relSCurve_precalculated(i, probs_array[j], targeted_removal=False, simulated=False, finite=True)[:i])
         #print("fin",fin)
         heatmap1[j][i-1] = ((fin-sim)**2).mean()
+maxf = np.max(heatmap1)
+print("max fin", maxf)
+medf = np.median(heatmap1)
+print("median fin", medf)
+
 
 heatmap1 = heatmap1.tolist()
 # print(heatmap1)
@@ -103,16 +108,20 @@ for i in range(1,total_n+1):
         for k in range(i):
             sim = sim + np.transpose(all_sim[:,k][:i])
         sim = sim / i
-        print(np.shape(sim))
+        #print(np.shape(sim))
         path_name = "{}_attack{}_n{}.npy".format("infRelSCurve", targeted_removal, i)
         path_name = os.path.join("data", "synthetic_data", path_name)
         all_inf = np.load(path_name)
         #print(np.shape(all_inf))
         inf = all_inf[j]
-        print("i", i, "j", j, "inf", inf)
+        #print("i", i, "j", j, "inf", inf)
         heatmap2[j][i-1] = ((inf-sim)**2).mean()
 
 heatmap2 = heatmap2.tolist()
+maxi = np.max(heatmap2)
+print("max inf", maxi)
+medi = np.median(heatmap2)
+print("median inf", medi)
 #xnodes, yprobs = np.meshgrid(nodes_array, probs_array)
 
 # plt.pcolormesh(xnodes, yprobs, heatmap2)
@@ -135,7 +144,7 @@ for i in range(1,total_n+1):
         for k in range(i):
             sim = sim + np.transpose(all_sim[:,k][:i])
         sim = sim / i
-        print(np.shape(sim))
+        #print(np.shape(sim))
         path_name = "{}_attack{}_n{}.npy".format("infRelSCurve", targeted_removal, i)
         path_name = os.path.join("data", "synthetic_data", path_name)
         all_inf = np.load(path_name)
@@ -157,20 +166,60 @@ heatmap3 = heatmap3.tolist()
 # plt.colorbar()
 # plt.savefig("mse difference")
 
+def add_colorbar(mappable):
+    from mpl_toolkits.axes_grid1 import make_axes_locatable
+    import matplotlib.pyplot as plt
+    last_axes = plt.gca()
+    ax = mappable.axes
+    fig = ax.figure
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    cbar = fig.colorbar(mappable, cax=cax)
+    plt.sca(last_axes)
+    return cbar
 
-fig, axs = plt.subplots(2, 2)
-axs[0, 0].pcolormesh(xnodes, yprobs, heatmap0)
-axs[0, 0].set_title('AUC')
-axs[0,0].set(xlabel='nodes', ylabel='p')
-axs[0, 1].pcolormesh(xnodes, yprobs, heatmap1)
-axs[0, 1].set_title('MSE Finite')
-axs[0,1].set(xlabel='nodes', ylabel='p')
-axs[1, 0].pcolormesh(xnodes, yprobs, heatmap2)
-axs[1, 0].set_title('MSE Difference')
-axs[1,0].set(xlabel='nodes', ylabel='p')
-axs[1, 1].pcolormesh(xnodes, yprobs, heatmap3)
-axs[1, 1].set_title('MSE Infinite')
-axs[1,1].set(xlabel='nodes', ylabel='p')
+
+fig , ( (ax1,ax2) , (ax3,ax4)) = plt.subplots(2, 2,sharex = True,sharey=True)
+z1_plot = ax1.pcolormesh(xnodes, yprobs, heatmap0, cmap = "Reds")
+
+z2_plot = ax2.pcolormesh(xnodes, yprobs, heatmap1, vmax=0.05)
+z3_plot = ax3.pcolormesh(xnodes, yprobs, heatmap3, cmap = "Blues")
+z4_plot = ax4.pcolormesh(xnodes, yprobs, heatmap2, vmax=0.2)
+add_colorbar(z1_plot)
+add_colorbar(z2_plot)
+add_colorbar(z3_plot)
+add_colorbar(z4_plot)
+ax1.set(xlabel='nodes', ylabel='p')
+ax2.set(xlabel='nodes', ylabel='p')
+ax3.set(xlabel='nodes', ylabel='p')
+ax4.set(xlabel='nodes', ylabel='p')
+ax1.set_title("AUC")
+ax2.set_title("MSE Finite")
+ax3.set_title("MSE Difference")
+ax4.set_title('MSE Infinite')
+
+plt.savefig("Fig 3 Final")
+
+# fig, axs = plt.subplots(2, 2)
+# axs[0, 0].pcolormesh(xnodes, yprobs, heatmap0)
+# axs[0, 0].set_title('AUC')
+# axs[0, 0].set(xlabel='nodes', ylabel='p')
+# axs[0, 1].pcolormesh(xnodes, yprobs, heatmap1, vmax = 0.05)
+# axs[0, 1].set_title('MSE Finite')
+# axs[0, 1].set(xlabel='nodes', ylabel='p')
+# axs[1, 0].pcolormesh(xnodes, yprobs, heatmap3)
+# axs[1, 0].set_title('MSE Difference')
+# axs[1, 0].set(xlabel='nodes', ylabel='p')
+# axs[1, 1].pcolormesh(xnodes, yprobs, heatmap2, vmax = .2)
+# axs[1, 1].set_title('MSE Infinite')
+# axs[1, 1].set(xlabel='nodes', ylabel='p')
+
+
+
+
+
+#im = axs[1, 1].imshow(heatmap2)
+#plt.colorbar(im, ax=axs[1, 1])
 
 # for ax in axs.flat:
     # ax.set(xlabel='nodes', ylabel='MSE')
@@ -178,5 +227,3 @@ axs[1,1].set(xlabel='nodes', ylabel='p')
 # Hide x labels and tick labels for top plots and y ticks for right plots.
 # for ax in axs.flat:
 #     ax.label_outer()
-
-plt.savefig("Fig 3 Final")
